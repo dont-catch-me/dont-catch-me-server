774e48f94d32b7366552c682208b64902db2b16b
iren <irenn.dev@gmail.com>
2021-10-17 21:53:34 +0900

    refactor: delete console log

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,10 +4,7 @@ const router = express.Router();
 router.post(
   "/api/result/:id",
   (req, res, next) => {
-    console.log(244);
-    console.log(req.body);
     const { record } = req.body;
-    console.log(record);
   },
 );
 
